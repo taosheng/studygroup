dc7821c0c2545a764cb9a47b66f76deae2a1a1b9
Tao-Sheng <taosheng.chen@gmail.com>
2012-08-11 21:44:27 +0800

    add simple example for music brainz xml web service

---
+++ src/net/studygroup/Test.java
@@ -0,0 +1,11 @@
+package net.studygroup;
+
+public class Test{
+	public static void main(String argv[]){
+		println("Test program");
+	}
+	private static void println(Object msg){
+		System.out.println(msg);
+	}
+}
+
